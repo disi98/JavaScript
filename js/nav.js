3ef96d095e2bd23db7c6e5cb839468903e462e87
Albert Disi Lungonzo <67419107+disi98@users.noreply.github.com>
2022-12-21 23:50:27 +0000

    update

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
-  // Toggle the nav links when the menu icon is clicked
-  $('.menu-icon').on('click', function() {
-    $('.nav-links').toggleClass('active');
-  });
-});
+const navbarToggler = document.querySelector('.navbar-toggler');
+const navbarNav = document.querySelector('.navbar-nav');
+
+function toggleNav() {
+  navbarNav.classList.toggle('show');
+}
+
+navbarToggler.addEventListener('click', toggleNav);
