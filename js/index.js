4445c7d330278ffe74d731efec154131bc893078
Albert Disi Lungonzo <67419107+disi98@users.noreply.github.com>
2022-12-21 22:59:52 +0000

    update

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Toggle the nav links when the menu icon is clicked
+    $('.menu-icon').on('click', function() {
+      $('.nav-links').toggleClass('active');
+    });
+  });
+  
