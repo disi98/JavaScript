27114c39fb9726dd95a6d984a5872885aa0372cf
Albert Disi Lungonzo <67419107+disi98@users.noreply.github.com>
2022-12-21 23:32:01 +0000

    update

--- js/nav.js
+++ js/nav.js
@@ -1,24 +1,2 @@
-$(document).ready(function() {
-  // Add a smooth scroll effect to the navigation links
-  $('nav a').on('click', function(event) {
-    if (this.hash !== '') {
-      event.preventDefault();
-
-      const hash = this.hash;
-
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function() {
-        window.location.hash = hash;
-      });
-    }
-  });
-
-  // Toggle the visibility of the nav items when the hamburger icon is clicked
-  $('.menu-icon').on('click', function() {
-    $('nav ul').slideToggle();
-  });
-});
-
-
-document.getElementById("add").textContent = 5;
+const messageElement = document.getElementById("message");
+messageElement.textContent = "Hello, JavaScript!";
