f95dc2d014717a09f40cf602409eb425d3c024f1
Albert Disi Lungonzo <67419107+disi98@users.noreply.github.com>
2022-12-22 08:50:49 +0000

    update

--- js/nav.js
+++ js/nav.js
@@ -4,3 +4,13 @@ const navbarNav = document.querySelector('.navbar-nav');
 function toggleNav() {
   navbarNav.classList.toggle('show');
 }
+
+const projectViewBtn = document.querySelector('.cta .btn');
+const targetSection = document.querySelector('#target-section');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  targetSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+projectViewBtn.addEventListener('click', scrollToSection);
